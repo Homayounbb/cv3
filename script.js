30b8bb1ba69ed626cf0d3aa825806c2ca2e1c0a4
Homayounbb <98332448+Homayounbb@users.noreply.github.com>
2022-06-25 09:45:41 +0430

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ document.querySelector(".menu").addEventListener("click", () => {
 
 const icons = document.querySelectorAll(".section-1-icons i");
 
+let i = 1;
+
 setInterval(() => {
+  i++;
+
   const icon = document.querySelector(".section-1-icons .change");
 
   icon.classList.remove("change");
-  icon.nextElementSibling.classList.add("change");
+
+  if (i > icons.length) {
+    icons[0].classList.add("change");
+    i = 1;
+  } else {
+    icon.nextElementSibling.classList.add("change");
+  }
 }, 4000);
